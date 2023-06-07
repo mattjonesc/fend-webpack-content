9dee6e377f4c6ef741278449088459debd7cd438
mattjonesc <matt.jones.c@gmail.com>
2023-06-07 13:23:23 -0400

    working on final lesson

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,12 +7,20 @@ module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules : [
             {   
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
