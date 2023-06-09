89abe36ca4a00a3a217512bbf125887b04a0532c
mattjonesc <matt.jones.c@gmail.com>
2023-06-09 18:20:02 -0400

    got it working first try

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,7 @@
+// Personal API Key for OpenWeatherMap API
+const apiKey = '&appid=23abe556d12b81861a4dd18bec071de8&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -7,12 +11,13 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch(url+'18104'+apiKey)
     .then(res => {
         return res.json()
     })
+    //this will post the resulting data to the webpage
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+        document.getElementById('results').innerHTML = data.name
     })
 }
 
